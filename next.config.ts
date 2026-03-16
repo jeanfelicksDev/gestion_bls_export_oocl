60f497b9f3d0ebfa41e145ac95dd8d55855dac18
jeanfelicksDev <jeanfelicks11@gmail.com>
2026-03-16 17:25:44 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    typescript: {
-          ignoreBuildErrors: true,
-    },
-    eslint: {
-          ignoreDuringBuilds: true,
-    }
+      typescript: {
+              ignoreBuildErrors: true,
+      },
+      eslint: {
+              ignoreDuringBuilds: true,
+      },
+      serverExternalPackages: ["@prisma/adapter-better-sqlite3", "better-sqlite3"],
 };
 
 export default nextConfig;
